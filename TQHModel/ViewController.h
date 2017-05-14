45a493b28a8c665b9a8242b903c56a3d4033b105
tankfice <1178241203@qq.com>
2017-05-14 18:17:11 +0800

    Initial Commit

---
+++ TQHModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TQHModel
+//
+//  Created by 汤清欢 on 2017/5/14.
+//  Copyright © 2017年 Hanlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TQHModel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TQHModel
+//
+//  Created by 汤清欢 on 2017/5/14.
+//  Copyright © 2017年 Hanlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
